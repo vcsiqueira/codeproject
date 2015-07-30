9098f80e0700e9e7ed7ae7bb1c24744a401a43e7
Vinicius Siqueira <vcsiqueira@MacBook-Pro-de-Vinicius.local>
2015-07-30 14:54:52 -0300

    criando rota com put

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,9 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('client','ClientController@index');
-
 Route::post('client','ClientController@store');
 Route::get('client/{id}','ClientController@show');
 Route::delete('client/{id}','ClientController@destroy');
+Route::put('client/{id}','ClientController@update');
